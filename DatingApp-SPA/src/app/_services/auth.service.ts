d2bea04ecc930dba0879397c3bbcd8c40e67ec98
Juan Agudelo <calijuma@gmail.com>
2020-07-29 17:44:17 -0400

    End of section 12

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -42,8 +42,8 @@ login(model: any){
     );
 }
 
-register(model: any){
-  return this.http.post(this.baseurl + 'register', model);
+register(user: User){
+  return this.http.post(this.baseurl + 'register', user);
 }
 
 loggedIn(){
--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -55,6 +55,12 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+        // update current photo
+        if (photo.isMain){
+          this.authService.changeMemberPhoto(photo.url);
+          this.authService.currentUser.photoUrl = photo.url;
+          localStorage.setItem('user', JSON.stringify(this.authService.currentUser));
+        }
       }
     };
   }
